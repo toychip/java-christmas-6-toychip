bc2776e9b5cb70f7938b49f51d169ecb540d8094
toychip <dlawnsgud427@naver.com>
2023-11-12 21:02:00 +0900

    feat: ChristmasController 초안 추가

---
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -0,0 +1,17 @@
+package christmas.controller;
+
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
+public class ChristmasController {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public ChristmasController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+    }
+}
