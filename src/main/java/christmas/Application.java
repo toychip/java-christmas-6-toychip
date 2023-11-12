798cdd83f0c3b14bac2afdf7454a455abee1e328
toychip <dlawnsgud427@naver.com>
2023-11-12 21:03:29 +0900

    feat: Application ChristmasController 사용

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,12 @@
 package christmas;
 
+import christmas.controller.ChristmasController;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        ChristmasController xmasController = new ChristmasController(new InputView(), new OutputView());
+        xmasController.run();
     }
 }
