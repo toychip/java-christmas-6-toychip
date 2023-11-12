f94d5c17209ab4b80d724cf0482b57662fb4d81b
toychip <dlawnsgud427@naver.com>
2023-11-13 08:39:04 +0900

    feat: Price 클래스 추가

---
+++ src/main/java/christmas/domain/menu/Price.java
@@ -0,0 +1,20 @@
+package christmas.domain.menu;
+
+public class Price {
+    private final int value;
+
+    public Price(int value) {
+        validate(value);
+        this.value = value;
+    }
+
+    private void validate(int value) {
+        if (value < 0) {
+            throw new IllegalArgumentException("[ERROR] 가격은 0원 이하일 수 없음");
+        }
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
