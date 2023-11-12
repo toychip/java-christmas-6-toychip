f229ae3d1bff4a9508719b99085ef3ec85fb9837
toychip <dlawnsgud427@naver.com>
2023-11-12 22:03:41 +0900

    feat: Name 클래스 추가

---
+++ src/main/java/christmas/domain/menu/Name.java
@@ -0,0 +1,18 @@
+package christmas.domain.menu;
+
+public class Name {
+    private final String name;
+
+    public Name(String name) {
+        validate(name);
+        this.name = name;
+    }
+
+    private void validate(String name) {
+        // ... 이름이 에피타이저, 메인, 디저트, 음료에 포함되어있는지 확인
+    }
+
+    public String getName() {
+        return name;
+    }
+}
