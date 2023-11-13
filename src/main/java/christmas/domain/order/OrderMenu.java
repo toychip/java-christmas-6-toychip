a6d7b58fa764ab1be3269df957f608ce93b4af7f
toychip <dlawnsgud427@naver.com>
2023-11-13 10:38:51 +0900

    feat: OrderMenu 클래스 추가

---
+++ src/main/java/christmas/domain/order/OrderMenu.java
@@ -0,0 +1,47 @@
+package christmas.domain.order;
+
+import christmas.domain.menu.Appetizer;
+import christmas.domain.menu.Desert;
+import christmas.domain.menu.Drink;
+import christmas.domain.menu.Main;
+import christmas.domain.menu.component.Name;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class OrderMenu {
+    private final String menuName;
+
+    public OrderMenu(String menuName) {
+        validate(menuName);
+        this.menuName = menuName;
+    }
+
+    private void validate(String userMenu) {
+        validateMenuExistence(userMenu);
+    }
+
+    private void validateMenuExistence(String userMenu) {
+        List<Name> allMenuNames = getAllMenuNames();
+        boolean isContains = allMenuNames.stream()
+                .anyMatch(name -> name.toString().equals(userMenu));
+
+        if (!isContains) {
+            throw new IllegalArgumentException("[ERROR] 메뉴에 없는 주문");
+        }
+    }
+
+    private List<Name> getAllMenuNames() {
+        return Stream.of(
+                        Appetizer.allName(),
+                        Desert.allName(),
+                        Drink.allName(),
+                        Main.allName()
+                ).flatMap(Collection::stream)
+                .toList();
+    }
+
+    public String getMenuName() {
+        return menuName;
+    }
+}
