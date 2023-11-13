7a99bd15d39f1f5c212357e1d606a20926ab7550
toychip <dlawnsgud427@naver.com>
2023-11-13 17:22:23 +0900

    test: 메뉴가 겹칠때 테스트

--- src/test/java/christmas/domain/order/OrdersTest.java
+++ src/test/java/christmas/domain/order/OrdersTest.java
@@ -3,6 +3,7 @@ package christmas.domain.order;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import christmas.exception.NotExistsMenuException;
+import christmas.exception.OrderDuplicateMenuException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -19,6 +20,19 @@ class OrdersTest {
 
         //when && then
         NotExistsMenuException e = Assertions.assertThrows(NotExistsMenuException.class,
+                () -> new Orders(userInput));
+        assertEquals(e.getMessage(), MESSAGE);
+    }
+
+    @Test
+    @DisplayName("메뉴가 겹칠 때 OrderDuplicateMenuException 발생하며 공식 오류메시지 출력")
+    void 메뉴가_겹칠때_테스트() {
+        //given
+        String userInput = "양송이스프-2, 양송이스프-1";
+
+        //when && then
+        OrderDuplicateMenuException e = Assertions.assertThrows(
+                OrderDuplicateMenuException.class,
                 () -> new Orders(userInput));
         assertEquals(e.getMessage(), MESSAGE);
     }
