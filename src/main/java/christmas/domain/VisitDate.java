0663b055c26dc7a3af8dc34b1155768d69559e8c
toychip <dlawnsgud427@naver.com>
2023-11-13 21:05:13 +0900

    feat: VisitDate String date int 형변환 메서드 추가

--- src/main/java/christmas/domain/VisitDate.java
+++ src/main/java/christmas/domain/VisitDate.java
@@ -7,9 +7,18 @@ public class VisitDate {
     private static final int DATE_MIN = 1;
     private static final int DATE_MAX = 31;
 
-    public VisitDate(final int date) {
+    public VisitDate(final String inputDate) {
+        int date = toInt(inputDate);
         validate(date);
         this.date = date;
+    }
+
+    private int toInt(String date) {
+        try {
+            return Integer.parseInt(date);
+        } catch (NumberFormatException e) {
+            throw new DateUnitException();
+        }
     }
 
     private void validate(final int date) {
