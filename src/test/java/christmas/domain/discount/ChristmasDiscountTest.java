5b55af1b9b6cc76b953c2fbc61a48ff1b746d28f
toychip <dlawnsgud427@naver.com>
2023-11-14 14:26:58 +0900

    test: 크리스마스 디데이 할인 - 날짜 잘못된 경우 테스트

--- src/test/java/christmas/domain/discount/ChristmasDiscountTest.java
+++ src/test/java/christmas/domain/discount/ChristmasDiscountTest.java
@@ -1,12 +1,15 @@
 package christmas.domain.discount;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import christmas.domain.VisitDate;
 import christmas.domain.menu.component.Price;
+import christmas.exception.discount.XmasDiscountUnitException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class ChristmasDiscountTest {
 
@@ -25,5 +28,19 @@ class ChristmasDiscountTest {
 
         // then
         assertEquals(discountValue, expectDiscountValue);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"26", "29", "30"})
+    @DisplayName("크리스마스 할인 생성2 - 크리스마스 디데이 할인 종료 후에 생성 시도시 XmasDiscountUnitException (내부 오류)")
+    void 크리스마스_할인_생성_테스트2(String userInput) {
+        // given
+        VisitDate date = new VisitDate(userInput);
+        Price originalPrice = new Price(10000);
+
+        // when
+        assertThrows(XmasDiscountUnitException.class,
+                () -> new ChristmasDiscount(date, originalPrice));
+
     }
 }
