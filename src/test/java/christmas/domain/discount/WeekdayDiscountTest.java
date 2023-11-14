338c99388f93fb86e04636cb3736f0c633980f69
toychip <dlawnsgud427@naver.com>
2023-11-14 17:24:15 +0900

    test: 평일 할인 클래스에 들어온 날짜가 평일인지 검증하는 테스트

--- src/test/java/christmas/domain/discount/WeekdayDiscountTest.java
+++ src/test/java/christmas/domain/discount/WeekdayDiscountTest.java
@@ -1,10 +1,12 @@
 package christmas.domain.discount;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import christmas.domain.VisitDate;
 import christmas.domain.menu.component.Price;
 import christmas.domain.order.Orders;
+import christmas.exception.discount.InvalidWeekDayException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -12,7 +14,24 @@ import org.junit.jupiter.params.provider.CsvSource;
 class WeekdayDiscountTest {
 
     @ParameterizedTest
-    @CsvSource("23, '아이스크림-3, 티본스테이크- 2, 초코케이크-3', 100000")
+    @CsvSource({"1", "2", "8", "9", "15", "16", "22", "23", "29", "30"})
+    @DisplayName("평일 날짜 검증, 실패시 InvalidWeekDayException")
+    void 평일_날짜_검증(String userDateInput) {
+
+        // given
+        String userOrdersInput = "아이스크림-3, 티본스테이크- 2, 초코케이크-3";
+        int value = 100000;
+        VisitDate visitDate = new VisitDate(userDateInput);
+        Orders orders = new Orders(userOrdersInput);
+        Price price = new Price(value);
+
+        // when && then
+        assertThrows(InvalidWeekDayException.class,
+                () -> new WeekdayDiscount(visitDate, orders, price));
+    }
+
+    @ParameterizedTest
+    @CsvSource("3, '아이스크림-3, 티본스테이크- 2, 초코케이크-3', 100000")
     @DisplayName("평일 할인 값 테스트")
     void 메뉴_개수에따른_평일_할인값(String userDateInput, String userOrdersInput, int value) {
 
