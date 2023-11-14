0069b30cc8ea33ae6190e79b8d1d15ac8f1d948a
toychip <dlawnsgud427@naver.com>
2023-11-14 14:19:08 +0900

    feat: 평일 할인 클래스 추가

--- src/main/java/christmas/domain/discount/WeekdayDiscount.java
+++ src/main/java/christmas/domain/discount/WeekdayDiscount.java
@@ -1,27 +1,64 @@
 package christmas.domain.discount;
 
 import christmas.domain.VisitDate;
+import christmas.domain.menu.Desert;
+import christmas.domain.menu.component.Name;
 import christmas.domain.menu.component.Price;
+import christmas.domain.order.Order;
+import christmas.domain.order.Orders;
+import java.util.List;
 
 public class WeekdayDiscount implements Discount{
 
+    private final VisitDate visitDate;
+    private final Orders orders;
+    private final Price originalPrice;
+    private final Price discountValue;
+
+    public WeekdayDiscount(VisitDate visitDate, Orders orders, Price originalPrice) {
+        this.visitDate = visitDate;
+        this.orders = orders;
+        this.originalPrice = originalPrice;
+        this.discountValue = discount();
+    }
+
     @Override
-    public int discount() {
-        return 0;
+    public Price discount() {
+        int value = calculateDiscount();
+        return new Price(value);
+    }
+
+    private int calculateDiscount() {
+        int count = orderContainsDesertCount();
+        return count * 2023;
+    }
+
+    private int orderContainsDesertCount() {
+        List<String> desertMenuNames = getDesertMenuNames();
+        return (int) orders.getOrders().stream()
+                .map(Order::orderMenuName)
+                .filter(desertMenuNames::contains)
+                .count();
+    }
+
+    private List<String> getDesertMenuNames() {
+        return Desert.allName().stream()
+                .map(Name::toString)
+                .toList();
     }
 
     @Override
     public VisitDate getVisitDate() {
-        return null;
+        return visitDate;
     }
 
     @Override
     public Price getOriginalPrice() {
-        return null;
+        return originalPrice;
     }
 
     @Override
     public Price getDiscountValue() {
-        return null;
+        return discountValue;
     }
 }
