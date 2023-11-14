a2b3bc80cdbfe250a6319a4938062cdcfbc76cb0
toychip <dlawnsgud427@naver.com>
2023-11-14 13:27:52 +0900

    feat: 특별 할인 추가

--- src/main/java/christmas/domain/discount/SpecialDiscount.java
+++ src/main/java/christmas/domain/discount/SpecialDiscount.java
@@ -5,23 +5,38 @@ import christmas.domain.menu.component.Price;
 
 public class SpecialDiscount implements Discount{
 
+    private final VisitDate visitDate;
+    private final Price originalPrice;
+    private final Price discountValue;
+
+    public SpecialDiscount(VisitDate visitDate, Price originalPrice) {
+        this.visitDate = visitDate;
+        this.originalPrice = originalPrice;
+        this.discountValue = calculateDiscount();
+    }
+
+    private Price calculateDiscount() {
+        int value = discount();
+        return new Price(value);
+    }
+
     @Override
     public int discount() {
-        return 0;
+        return 1000;
     }
 
     @Override
     public VisitDate getVisitDate() {
-        return null;
+        return visitDate;
     }
 
     @Override
     public Price getOriginalPrice() {
-        return null;
+        return originalPrice;
     }
 
     @Override
     public Price getDiscountValue() {
-        return null;
+        return discountValue;
     }
 }
