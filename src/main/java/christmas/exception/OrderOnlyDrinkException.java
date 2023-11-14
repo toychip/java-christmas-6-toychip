a7617eda54eed9301e478c368d54fb2b68919a5a
toychip <dlawnsgud427@naver.com>
2023-11-14 09:24:59 +0900

    teat: 음료만 주문했을 때 테스트

--- src/main/java/christmas/exception/OrderOnlyDrinkException.java
+++ src/main/java/christmas/exception/OrderOnlyDrinkException.java
@@ -1,7 +1,7 @@
 package christmas.exception;
 
 public class OrderOnlyDrinkException extends IllegalArgumentException {
-    private static final String MESSAGE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+    private static final String MESSAGE = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
     public OrderOnlyDrinkException() {
         super(MESSAGE);
     }
--- src/test/java/christmas/domain/order/OrdersTest.java
+++ src/test/java/christmas/domain/order/OrdersTest.java
@@ -7,6 +7,7 @@ import christmas.domain.VisitDate;
 import christmas.exception.DateUnitException;
 import christmas.exception.NotExistsMenuException;
 import christmas.exception.OrderDuplicateMenuException;
+import christmas.exception.OrderOnlyDrinkException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -17,11 +18,11 @@ class OrdersTest {
     private static final String ORDER_ERROR_MESSAGE = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
     private static final String DATE_ERROR_MESSAGE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
 
-    @Test
+    @ParameterizedTest
     @DisplayName("메뉴에 없는 주문 생성시 NotExistsMenuException 발생하며 공식 주문 오류메시지 출력")
-    void 메뉴에_없는_주문_테스트() {
+    @ValueSource(strings = {"블루와인-2, 양송이스프-1", "초콜릿-2, 사탕-1"})
+    void 메뉴에_없는_주문_테스트(String userInput) {
         //given
-        String userInput = "블루와인-2, 양송이스프-1";
 
         //when && then
         NotExistsMenuException e = assertThrows(NotExistsMenuException.class,
@@ -29,12 +30,11 @@ class OrdersTest {
         assertEquals(e.getMessage(), ORDER_ERROR_MESSAGE);
     }
 
-    @Test
+    @ParameterizedTest
     @DisplayName("메뉴가 겹칠 때 OrderDuplicateMenuException 발생하며 공식 주문 오류메시지 출력")
-    void 메뉴가_겹칠때_테스트() {
+    @ValueSource(strings = {"양송이스프-2, 양송이스프-1", "티본스테이크-2, 양송이스프-1, 티본스테이크-1"})
+    void 메뉴가_겹칠때_테스트(String userInput) {
         //given
-        String userInput = "양송이스프-2, 양송이스프-1";
-
         //when && then
         OrderDuplicateMenuException e = assertThrows(
                 OrderDuplicateMenuException.class,
@@ -42,9 +42,9 @@ class OrdersTest {
         assertEquals(e.getMessage(), ORDER_ERROR_MESSAGE);
     }
 
+    @ParameterizedTest
     @DisplayName("날짜가 올바르지 않을 때 DateUnitException 발생하며 공식 날짜 오류메시지 출력")
     @ValueSource(strings = {"32", "ㄱㄴㅇ", "2147483648"})
-    @ParameterizedTest
     void 날짜가_올바르지_않을_때(String userInput) {
 
         //when && then
@@ -54,4 +54,14 @@ class OrdersTest {
         assertEquals(e.getMessage(), DATE_ERROR_MESSAGE);
     }
 
+//    @Test
+    @ParameterizedTest
+    @DisplayName("음료만 주문했을 때 OrderOnlyDrinkException 발생하며 공식 주문 오류 메시지 출력")
+    @ValueSource(strings = {"레드와인-3, 샴페인- 1", "제로콜라-3, 레드와인- 1"})
+    void 음료만_주문했을_때(String userInput) {
+        OrderOnlyDrinkException e = assertThrows(
+                OrderOnlyDrinkException.class,
+                () -> new Orders(userInput));
+        assertEquals(ORDER_ERROR_MESSAGE, e.getMessage());
+    }
 }
