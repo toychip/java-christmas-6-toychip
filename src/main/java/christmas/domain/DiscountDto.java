709eff9e8e885d99aa00aab52f828d059187ab29
toychip <dlawnsgud427@naver.com>
2023-11-15 14:26:17 +0900

    refactor: DiscounDto를 record로 변경

--- src/main/java/christmas/domain/DiscountDto.java
+++ src/main/java/christmas/domain/DiscountDto.java
@@ -10,25 +10,11 @@ import christmas.domain.discount.WeekendDiscount;
 import christmas.domain.menu.component.Price;
 import christmas.domain.order.Orders;
 
-public class DiscountDto {
+public record DiscountDto(ChristmasDiscount christmasDiscount, GiftMenuEvent giftMenuEvent,
+                          SpecialDiscount specialDiscount, WeekdayDiscount weekdayDiscount,
+                          WeekendDiscount weekendDiscount) {
 
-    private final ChristmasDiscount christmasDiscount;
-    private final GiftMenuEvent giftMenuEvent;
-    private final SpecialDiscount specialDiscount;
-    private final WeekdayDiscount weekdayDiscount;
-    private final WeekendDiscount weekendDiscount;
-
-    public DiscountDto(ChristmasDiscount christmasDiscount, GiftMenuEvent giftMenuEvent,
-                       SpecialDiscount specialDiscount, WeekdayDiscount weekdayDiscount,
-                       WeekendDiscount weekendDiscount) {
-        this.christmasDiscount = christmasDiscount;
-        this.giftMenuEvent = giftMenuEvent;
-        this.specialDiscount = specialDiscount;
-        this.weekdayDiscount = weekdayDiscount;
-        this.weekendDiscount = weekendDiscount;
-    }
-
-    protected static ChristmasDiscount judgeXmasDiscount(VisitDate visitDate) {
+    static ChristmasDiscount judgeXmasDiscount(VisitDate visitDate) {
         int date = visitDate.getDate();
         if (date <= 25) {
             return new ChristmasDiscount(visitDate);
@@ -36,53 +22,32 @@ public class DiscountDto {
         return null;
     }
 
-    protected static GiftMenuEvent judgeGiftMenuEvent(Price price){
+    static GiftMenuEvent judgeGiftMenuEvent(Price price) {
         if (price.getValue() >= 120000) {
             return new GiftMenuEvent(price);
         }
         return null;
     }
 
-    protected static SpecialDiscount judgeSpecialDiscount(VisitDate visitDate) {
+    static SpecialDiscount judgeSpecialDiscount(VisitDate visitDate) {
         if (DecemberCalendar.matchStar(visitDate)) {
             return new SpecialDiscount(visitDate);
         }
         return null;
     }
 
-    protected static WeekdayDiscount judgeWeekdayDiscount(VisitDate visitDate, Orders orders) {
+    static WeekdayDiscount judgeWeekdayDiscount(VisitDate visitDate, Orders orders) {
         if (DecemberCalendar.matchWeekday(visitDate)) {
             return new WeekdayDiscount(visitDate, orders);
         }
         return null;
     }
 
-    protected static WeekendDiscount judgeWeekendDiscount(VisitDate visitDate, Orders orders) {
+    static WeekendDiscount judgeWeekendDiscount(VisitDate visitDate, Orders orders) {
         if (DecemberCalendar.matchWeekend(visitDate)) {
             return new WeekendDiscount(visitDate, orders);
         }
         return null;
     }
-
-    public ChristmasDiscount getChristmasDiscount() {
-        return christmasDiscount;
-    }
-
-    public GiftMenuEvent getGiftMenuEvent() {
-        return giftMenuEvent;
-    }
-
-    public SpecialDiscount getSpecialDiscount() {
-        return specialDiscount;
-    }
-
-    public WeekdayDiscount getWeekdayDiscount() {
-        return weekdayDiscount;
-    }
-
-    public WeekendDiscount getWeekendDiscount() {
-        return weekendDiscount;
-    }
-
 
 }
