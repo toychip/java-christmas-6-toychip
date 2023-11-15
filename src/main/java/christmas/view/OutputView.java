dc86348019b558a5d3b431f152c4af48cd06a17b
toychip <dlawnsgud427@naver.com>
2023-11-15 15:21:56 +0900

    feat: OutputView 증정 메뉴 출력 method 추가

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -3,45 +3,60 @@ package christmas.view;
 public class OutputView {
     public void introduce() {
         String message = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
-        print(message);
+        printMessage(message);
     }
 
     public void preview(int date) {
         String message = String.format("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!", date);
-        print(message);
+        printMessage(message);
     }
 
     public void orderMenuTitle() {
         String message = "<주문 메뉴>";
-        print(message);
+        printMessage(message);
     }
 
-    public void preDiscountTitle() {
+    public void preDiscount(int amount) {
         String message = "<할인 전 총주문 금액>";
-        print(message);
+        printMessage(message);
+        printAmount(amount);
+    }
+
+    public void giftMenu() {
+        String message = "<증정 메뉴>";
+        printMessage(message);
+    }
+
+    public void printGiftMenu(String name, int quantity) {
+        String message = name + quantity + "개";
+        printMessage(message);
     }
 
     public void benefitTitle() {
         String message = "<혜택 내역>";
-        print(message);
+        printMessage(message);
     }
 
     public void totalDiscountTitle() {
         String message = "<총혜택 금액>";
-        print(message);
+        printMessage(message);
     }
 
     public void postDiscountTitle() {
         String message = "<할인 후 예상 결제 금액>";
-        print(message);
+        printMessage(message);
     }
 
     public void eventBadgeTitle() {
         String message = "<12월 이벤트 배지>";
-        print(message);
+        printMessage(message);
     }
 
-    private void print(String message) {
+    private void printMessage(String message) {
         System.out.println(message);
+    }
+
+    private void printAmount(int amount) {
+        System.out.println(amount + "원");
     }
 }
