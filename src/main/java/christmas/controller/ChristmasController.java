8d2868f052d9b0958c9abe989e2578c0796f6e07
toychip <dlawnsgud427@naver.com>
2023-11-15 19:54:56 +0900

    feat: ChristmasController 할인내역 method 추가

--- src/main/java/christmas/controller/ChristmasController.java
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -1,5 +1,7 @@
 package christmas.controller;
 
+import static christmas.static_class.CommonStatic.NONE_NAME;
+
 import christmas.domain.DiscountDetailDto;
 import christmas.domain.GiftMenuDto;
 import christmas.domain.MoneyManagement;
@@ -80,7 +82,80 @@ public class ChristmasController {
     }
 
     private void benefitDetail() {
+        outputView.benefitTitle();
+        List<DiscountDetailDto> discountDetails = moneyManagement.getDiscountDetails();
+
+        detailDiscountXmas(discountDetails);
+        detailDiscountWeekday(discountDetails);
+        detailDiscountWeekend(discountDetails);
+        detailDiscountSpecial(discountDetails);
+        detailGiftMenu(discountDetails);
+        detailNothing();
     }
+
+    private void detailDiscountXmas(List<DiscountDetailDto> discountDetails) {
+        int christmasDiscountValue = discountDetails.stream()
+                .filter(type -> type.discountName().name().equals("ChristmasDiscount"))
+                .mapToInt(type -> type.discountPrice().value())
+                .sum();
+        if (christmasDiscountValue != 0) {
+            outputView.benefitDetailXmas(christmasDiscountValue);
+        }
+    }
+
+    private void detailDiscountWeekday(List<DiscountDetailDto> discountDetails) {
+        int weekdayDiscountValue = discountDetails.stream()
+                .filter(type -> type.discountName().name().equals("WeekdayDiscount"))
+                .mapToInt(type -> type.discountPrice().value())
+                .sum();
+
+        if (weekdayDiscountValue != 0) {
+            outputView.benefitWeekday(weekdayDiscountValue);
+        }
+    }
+
+    private void detailDiscountWeekend(List<DiscountDetailDto> discountDetails) {
+        int weekendDiscountValue = discountDetails.stream()
+                .filter(type -> type.discountName().name().equals("WeekendDiscount"))
+                .mapToInt(type -> type.discountPrice().value())
+                .sum();
+
+        if (weekendDiscountValue != 0) {
+            outputView.benefitWeekend(weekendDiscountValue);
+        }
+    }
+
+    private void detailDiscountSpecial(List<DiscountDetailDto> discountDetails) {
+        int specialDiscountValue = discountDetails.stream()
+                .filter(type -> type.discountName().name().equals("SpecialDiscount"))
+                .mapToInt(type -> type.discountPrice().value())
+                .sum();
+
+        if (specialDiscountValue != 0) {
+            outputView.benefitSpecial(specialDiscountValue);
+        }
+    }
+
+    private void detailGiftMenu(List<DiscountDetailDto> discountDetails) {
+        int giftMenuEventValue = discountDetails.stream()
+                .filter(type -> type.discountName().name().equals("GiftMenuEvent"))
+                .mapToInt(type -> type.discountPrice().value())
+                .sum();
+
+        if (giftMenuEventValue != 0) {
+            outputView.benefitGiftEvent(giftMenuEventValue);
+        }
+    }
+
+    private void detailNothing() {
+        Price totalDiscountPrice = moneyManagement.getTotalDiscountPrice();
+        int totalDiscountValue = totalDiscountPrice.value();
+
+        if (totalDiscountValue == 0) {
+            outputView.printMessage(NONE_NAME);
+        }
+    }
+
 
     private void totalBenefitPrice() {
     }
