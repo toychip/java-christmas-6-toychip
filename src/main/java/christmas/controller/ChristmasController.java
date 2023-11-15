e404d5945af62b00b08ab6135d654cb868a2fc44
toychip <dlawnsgud427@naver.com>
2023-11-15 19:41:05 +0900

    feat: ChristmasController VisitDate 초기화

--- src/main/java/christmas/controller/ChristmasController.java
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -1,17 +1,43 @@
 package christmas.controller;
 
+import christmas.domain.DiscountDetailDto;
+import christmas.domain.GiftMenuDto;
+import christmas.domain.MoneyManagement;
+import christmas.domain.date.VisitDate;
+import christmas.domain.discount.EventBadge;
+import christmas.domain.menu.component.Name;
+import christmas.domain.menu.component.Price;
+import christmas.domain.order.Orders;
 import christmas.view.InputView;
 import christmas.view.OutputView;
+import java.util.List;
 
 public class ChristmasController {
     private final InputView inputView;
     private final OutputView outputView;
+    private final VisitDate visitDate;
+    private final Orders orders;
+    private final MoneyManagement moneyManagement;
 
     public ChristmasController(final InputView inputView, final OutputView outputView) {
         this.inputView = inputView;
         this.outputView = outputView;
+        visitDate = initVisitDate();
+        orders = initOrders();
+        moneyManagement = initMoneyManagement();
     }
 
-    public void run() {
+    private VisitDate initVisitDate() {
+        String userDate = inputView.readDate();
+        return new VisitDate(userDate);
     }
+
+    private Orders initOrders() {
+        return null;
+    }
+
+    private MoneyManagement initMoneyManagement() {
+        return null;
+    }
+
 }
